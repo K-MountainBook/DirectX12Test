4d6a5fbfce08f9822448fa5d17c1e270fc00237e
K-MountainBook <keita.yamamoto.1009@gmail.com>
2026-04-24 09:56:44 +0900

    202604240956

--- Scene.cpp
+++ Scene.cpp
@@ -2,14 +2,30 @@
 #include "Engine.h"
 #include "App.h"
 #include <d3d12.h>
+#include "SharedStruct.h"
 
 Scene* g_Scene;
 
+using namespace DirectX;
+
 bool Scene::init()
 {
-    printf("シーンの初期化に成功\n");
+	Vertex verticles[3] = {};
 
-    return true;
+	verticles[0].Position = XMFLOAT3(-1.0f, -1.0f, -1.0f);
+	verticles[0].Color = XMFLOAT4(0.0f, 0.0f, 1.0f, 1.0f);
+
+	verticles[1].Position = XMFLOAT3(1.0f, -1.0f, 0.0f);
+	verticles[1].Color = XMFLOAT4(0.0f, 1.0f, 0.0f, 1.0f);
+
+	verticles[2].Position = XMFLOAT3(0.0f, 1.0f, 0.0f);
+	verticles[2].Color = XMFLOAT4(1.0f, 0.0f, 0.0f, 1.0f);
+
+
+
+	printf("シーンの初期化に成功\n");
+
+	return true;
 }
 
 void Scene::Update()
